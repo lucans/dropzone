6e4bafff118c065795ee24fd96c8be96ed0443f8
lucans <luuucans@gmail.com>
2017-06-20 00:44:30 -0300

    ó que óooo

--- demo/upload_1.php
+++ demo/upload_1.php
@@ -1,12 +1,19 @@
 <?php 
 
+session_start();
+
 chmod('C:\xampp\htdocs\dropzone\imagens', 0777);
 
-move_uploaded_file($_FILES['file']['tmp_name'], "C:\xampp\htdocs\dropzone\imagens");
+move_uploaded_file($_FILES['file']['tmp_name'], 'C:\xampp\htdocs\dropzone\imagens/' . $_FILES['file']['name']);
 
-rename('imagens', $_FILES['file']['name']);
 
-$file = file_get_contents(['file']['name']);
 
+$dir = opendir('C:\xampp\htdocs\dropzone\imagens/');
+
+$arquivo .= readdir($dir);
+
+// $file = file_get_contents($_FILES['file']['name']);
+
+echo $arquivo;
 
 ?>
