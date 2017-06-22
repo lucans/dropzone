d20df368f5e9bd22e8325531699236eefb30660d
lucans <luuucans@gmail.com>
2017-06-22 00:27:47 -0300

    great step

--- demo/upload_1.php
+++ demo/upload_1.php
@@ -1,19 +1,61 @@
-<?php 
+<?
 
-session_start();
+error_reporting(false);
 
-chmod('C:\xampp\htdocs\dropzone\imagens', 0777);
+extract($_REQUEST);
 
-move_uploaded_file($_FILES['file']['tmp_name'], 'C:\xampp\htdocs\dropzone\imagens/' . $_FILES['file']['name']);
+switch ($func) {
+	case 'upload':
+		Css::upload();
+		break;
+	case 'clean':
+		Css::clean();
+		break;
+	default:
+		echo "case def";
+		break;
+}
 
+class Css{
 
+	public function upload(){
 
-$dir = opendir('C:\xampp\htdocs\dropzone\imagens/');
+		chmod('C:\xampp\htdocs\dropzone\imagens', 0777);
 
-$arquivo .= readdir($dir);
+		move_uploaded_file($_FILES['file']['tmp_name'], 'C:\xampp\htdocs\dropzone\imagens/' . $_FILES['file']['name']);
 
-// $file = file_get_contents($_FILES['file']['name']);
+		echo "success";
+	}	
 
-echo $arquivo;
+	public function clean(){		
+
+		$dir = opendir('C:\xampp\htdocs\dropzone\imagens/');
+
+		$arquivo = readdir($dir);
+
+		while (false !== ($file = readdir($dir))) {
+			// echo file_get_contents('C:\xampp\htdocs\dropzone\imagens/' . $file);
+	        $final_file .= file_get_contents('C:\xampp\htdocs\dropzone\imagens/' . $file);	        
+	    }
+
+	    $final_file = strip_tags($final_file);
+		$final_file = str_replace(' ', '', $final_file);
+
+		// remmove comments
+	    $final_file = preg_replace('#/\*.*?\*/#s', '', $final_file);
+
+	    // Remove whitespace
+	    $final_file = preg_replace('/\s*([{}|:;,])\s+/', '$1', $final_file);
+
+	    // Remove trailing whitespace at the start
+	    $final_file = preg_replace('/\s\s+(.*)/', '$1', $final_file);
+	    
+	    // Remove unnecesairy ;'s
+	    $final_file = str_replace(';}', '}', $final_file);
+
+		echo $final_file;
+	}
+	
+}
 
 ?>
